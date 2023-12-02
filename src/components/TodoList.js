c413eb77008df66d999ee0d0b9292f0f5c3da1e6
datoi <dato.tadiashvili13@gmail.com>
2023-12-02 14:05:20 +0400

    added local storage saving

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,46 +1,73 @@
-import {useState} from "react";
+import React, { useState, useEffect } from "react";
 import TodoItem from "./todoitem/TodoItem";
 
 function TodoList() {
-    const [add, setAdd] = useState([])
-    const [value, setValue] = useState('')
-    const [count, setCount] = useState(0)
-    const [time, setTime] = useState(24)
+    const [add, setAdd] = useState([]);
+    const [value, setValue] = useState("");
+    const [count, setCount] = useState(0);
+    const [time, setTime] = useState(24);
     const [showButton, setShowButton] = useState(true);
 
+    useEffect(() => {
+        const savedAdd = JSON.parse(localStorage.getItem("todos")) || [];
+        const savedCountdown = JSON.parse(localStorage.getItem("countdown")) || { time: 24, showButton: true };
+
+        setAdd(savedAdd);
+        setCount(savedAdd.length);
+        setTime(savedCountdown.time);
+        setShowButton(savedCountdown.showButton);
+    }, []);
+
     const handleChange = (event) => {
-        setValue(event.target.value)
-    }
-    const countDown = () => {
-            setTimeout(() => {
-                const timer = setInterval(() => {
+        setValue(event.target.value);
+    };
 
-                    setTime(prevTime => (prevTime > 0 ? prevTime - 1 : prevTime));
-                }, 2000*30*24);
-                setTimeout(() => {
-                    clearInterval(timer);
-                    setAdd([]);
-                    setCount(count - count)
-                    setShowButton(true)
-                    setTime(10)
-                }, 2000*30*24);
-            }, 0);
-        setShowButton(!showButton);
-
-    }
     const addTodo = () => {
         if (add.length < 10) {
-            setAdd([...add, value])
-            setValue('')
-            setCount(count + 1)
-
+            const newTodos = [...add, value];
+            setAdd(newTodos);
+            setValue("");
+            setCount(newTodos.length);
+            localStorage.setItem("todos", JSON.stringify(newTodos));
         }
     };
+
     const removeTodo = (todoToRemove) => {
-        const newTodo = add.filter(todo => todo !== todoToRemove)
-        setAdd(newTodo)
-        setCount(count - 1)
-    }
+        const newTodo = add.filter((todo) => todo !== todoToRemove);
+        setAdd(newTodo);
+        setCount(newTodo.length);
+        localStorage.setItem("todos", JSON.stringify(newTodo));
+    };
+
+    const countDown = () => {
+        setTimeout(() => {
+            const timer = setInterval(() => {
+                setTime((prevTime) => (prevTime > 0 ? prevTime - 1 : prevTime));
+            }, 2000 * 30 * 24);
+
+            setTimeout(() => {
+                clearInterval(timer);
+                setAdd([]);
+                setCount(0);
+                setShowButton(true);
+                setTime(10);
+                localStorage.removeItem("todos");
+                localStorage.setItem("countdown", JSON.stringify({ time: 10, showButton: true }));
+            }, 2000 * 30 * 24);
+        }, 0);
+
+        setShowButton(!showButton);
+        localStorage.setItem("countdown", JSON.stringify({ time, showButton: !showButton }));
+    };
+    const resetClick = () => {
+        localStorage.clear();
+        setAdd([]);
+        setCount(0);
+        setShowButton(true);
+        setTime(24);
+    };
+
+
     return (
         <div>
             <TodoItem
@@ -53,9 +80,10 @@ function TodoList() {
                 time={time}
                 countDown={countDown}
                 showButton={showButton}
+                resetClick={resetClick}
             />
         </div>
-    )
+    );
 }
 
-export default TodoList
+export default TodoList;
--- src/components/todoitem/TodoItem.js
+++ src/components/todoitem/TodoItem.js
@@ -1,35 +1,40 @@
 import './TodoItem.css'
 
 function TodoItem(props) {
-    const {showButton,countDown, time, count, handleChange, value, addTodo, add, removeTodo} = props
+    const {resetClick, showButton, countDown, time, count, handleChange, value, addTodo, add, removeTodo} = props
     return (
-        <div className='App'>
-            <h1>Day Plan</h1>
+        <body>
+        <button className='refreshButton' onClick={resetClick}>Refresh</button>
+        <div className='front'>
+            <div className='App'>
+                <h1>Day Plan</h1>
 
-            <div className="container">
-            <input className='input' value={value} onChange={handleChange} type="text"/>
-            <button className='button1' onClick={addTodo}>add</button>
-            <div className='todoList'>
-                {add.map((todo, index) => (
-                    <p className='p' key={index}>{todo}
-                        <button className='button2' onClick={() => removeTodo(todo)}>X</button>
-                    </p>
-                ))}
-            </div>
-            <div className="progress" data-label={`${count * 10}%`}>
+                <div className="container">
+                    <input className='input' value={value} onChange={handleChange} type="text"/>
+                    <button className='button1' onClick={addTodo}>add</button>
+                    <div className='todoList'>
+                        {add.map((todo, index) => (
+                            <p className='p' key={index}>{todo}
+                                <button className='button2' onClick={() => removeTodo(todo)}>X</button>
+                            </p>
+                        ))}
+                    </div>
+                    <div className="progress" data-label={`${count * 10}%`}>
           <span className="value" style={{width: `${count * 10}%`}}>
             <span className="percent">{count}</span>
           </span>
 
-            </div>
-            <p>Maximum 10</p>
-            </div>
-            <div className='countDown'>
-                {showButton && <button className='button1' onClick={countDown}>Start Plan Timer</button>}
-                <h3>{time} hours remaining</h3>
-            </div>
+                    </div>
+                    <p>Maximum 10</p>
+                </div>
+                <div className='countDown'>
+                    {showButton && <button className='button1' onClick={countDown}>Start Plan Timer</button>}
+                    <h3>{time} hours remaining</h3>
+                </div>
 
+            </div>
         </div>
+        </body>
     )
 }
 
