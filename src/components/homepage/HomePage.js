81994b4cada3eaad12f51fd5288c758260da156f
datoi <dato.tadiashvili13@gmail.com>
2023-12-09 02:05:39 +0400

    starwars themes

--- src/components/homepage/HomePage.js
+++ src/components/homepage/HomePage.js
@@ -8,49 +8,75 @@ const HomePage = () => {
         selectedVariantName,
         selectedVariantDetails,
         selectedVariantImage,
+        selectedVariantBackground,
         handleVariantClick,
         variants,
     } = Login();
+    const bodyStyle = {
+        backgroundImage: selectedVariant ? `url(${process.env.PUBLIC_URL}/${selectedVariantImage})` : 'none',
+    };
 
     return (
-        <div className="container">
-            <div className="left-panel">
-                <h2>Choose a Variant:</h2>
-                <ul>
-                    {variants.map((item, index) => (
-                        <button key={index} onClick={() => handleVariantClick(item.variant, item.image)}>
-                            <strong>{item.variantName}</strong>
-                        </button>
-                    ))}
-                </ul>
-            </div>
-            {selectedVariant? (
-                <div className="right-panel">
-                    <div>
-                        <p>
-                            You have selected: <strong>{selectedVariantName}</strong>
-                        </p>
-                        <div className='description'>
-                            <div>
+        <body style={bodyStyle}>
+        <div className='bigBox'>
+            <div className="container">
+                <div className="left-panel">
+                    <h2 style={{marginTop: '0'}}>Choose a Variant:</h2>
+                    <ul>
+                        {variants.map((item, index) => (
+                            <button key={index}
+                                    onClick={() => handleVariantClick(item.variant, item.background, item.image)}>
+                                <strong>{item.variantName}</strong>
+                            </button>
+                        ))}
+                    </ul>
+                </div>
+                {selectedVariant ? (
+                    <div className="right-panel">
+                        <div>
+                            <p>
+                                You have selected: <strong>{selectedVariantName}</strong>
+                            </p>
+                            <div className='description'>
                                 <div>
-                                    <h3>Details:</h3>
-                                    <p>Name: {selectedVariantDetails.name}</p>
-                                    <p>Height: {selectedVariantDetails.height}</p>
-                                    <p>Mass: {selectedVariantDetails.mass}</p>
-                                    <p>Hair Color: {selectedVariantDetails.hairColor}</p>
+                                    <div>
+                                        <h3>Details:</h3>
+                                        <p>Name: {selectedVariantDetails.name}</p>
+                                        <p>Height: {selectedVariantDetails.height}</p>
+                                        <p>Mass: {selectedVariantDetails.mass}</p>
+                                    </div>
+                                </div>
+                                <div>
+                                    <img src={`${process.env.PUBLIC_URL}/${selectedVariantBackground}`}
+                                         style={{width: '150px', height: '150px'}}
+                                         alt="Selected Variant"/>
                                 </div>
                             </div>
-                            <div>
-                                <img src={`${process.env.PUBLIC_URL}/${selectedVariantImage}`}
-                                     style={{ width: '200px', height: '200px' }}
-                                     alt="Selected Variant"/>
-                            </div>
                         </div>
+                        <div style={{textAlign: 'center'}}>
+                            <button>select</button>
+                        </div>
+                    </div>) : null}
+            </div>
+            <div className="star-wars-body">
+                <div className="story-container">
+                    <div className="crawl-container">
+                        <p className='story'>
+                            May the Force be with you, young Jedi, as you traverse the boundless expanse of the galaxy,
+                            where the celestial bodies bear witness to an eternal conflict between the forces of light
+                            and the shadows of the dark side. Amidst the cosmic ballet of stars, your destiny unfolds as
+                            Luke Skywalker, a beacon of hope in the vastness of uncertainty. The weight of the Rebellion
+                            and the collective aspirations of countless beings converge upon your shoulders, and in this
+                            pivotal moment, meticulous preparation becomes not only advisable but an absolute
+                            imperative. Let us delve deeper into the profound significance of each task on your
+                            checklist, understanding how these seemingly mundane to-dos become the linchpins of survival
+                            for the galaxy's last hope
+                        </p>
                     </div>
                 </div>
-            ) : null}
-            <button>select</button>
+            </div>
         </div>
+        </body>
     );
 };
 
--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,40 +1,39 @@
 import axios from 'axios';
 import { useState } from 'react';
-
 const Login = () => {
     const [selectedVariant, setSelectedVariant] = useState('');
     const [selectedVariantName, setSelectedVariantName] = useState('');
     const [selectedVariantDetails, setSelectedVariantDetails] = useState({});
+    const [selectedVariantBackground, setSelectedVariantBackground] = useState('');
     const [selectedVariantImage, setSelectedVariantImage] = useState('');
 
-    const handleVariantClick = async (variant, image) => {
+    const handleVariantClick = async (variant, image, background) => {
         setSelectedVariant(variant);
-
         const response = await axios.get(`https://swapi.dev/api/${variant}`);
         const data = response.data;
         let info = {
             name: data.name,
             height: data.height,
             mass: data.mass,
-            hairColor: data.hair_color,
         };
-
         setSelectedVariantDetails(info);
         setSelectedVariantName(info.name);
-        setSelectedVariantImage(image);
+        setSelectedVariantBackground(background);
+        setSelectedVariantImage(image)
     };
 
     const variants = [
-        { variant: 'people/1/', variantName: 'Skywalker', image: 'skywalker.png' },
-        { variant: 'people/2/', variantName: 'C-3PO', image: 'C-3PO_EP3.webp' },
-        { variant: 'people/3/', variantName: 'R2-D2', image: 'r2d2.gif' },
-        { variant: 'people/4/', variantName: 'Darth Vader', image: 'darthvader.gif' },
+        { variant: 'people/1/', variantName: 'Sky walker', background: 'lukeSkyWalkerBackground.jpg', image: 'skywalker.png' },
+        { variant: 'people/10/', variantName: 'Obi-Wan', background: 'C3POBackground.jpg',image: 'ObiWan.png' },
+        { variant: 'people/3/', variantName: 'R2-D2', background: 'R2D2Background.png',image: 'r2d2.gif' },
+        { variant: 'people/4/', variantName: 'Darth Vader', background: 'darthVaderBackground.png',image: 'darthvader.gif' },
     ];
 
     return {
         selectedVariant,
         selectedVariantName,
         selectedVariantDetails,
+        selectedVariantBackground,
         selectedVariantImage,
         handleVariantClick,
         variants,
